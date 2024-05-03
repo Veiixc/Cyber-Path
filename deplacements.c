b536f1c21f03a381d6444a359dece0b1c14377e6
Mohamine2 <mohamed.bouazzaoui093@gmail.com>
2024-05-03 15:43:04 +0200

    Deplacements

---
+++ deplacements.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include "./struct.h"
+
+void deplacementsJoueur(CASE** grid,int* robotRow,int* robotCol){ //La fonction prend en paramètres les coordonnées du joueur et la grille
+    int direction;
+    printf("Wich direction:\n'z'=UP\n'q'=DOWN\n's'=LEFT\n'd'=RIGHT\n");
+    scanf("%d",&direction);
+    // Vérification de la direction saisie
+    switch(direction) {
+        case 'z': // aller en haut
+            if (grid[*robotRow + 1][*robotCol].state == IS_EMPTY) {
+                grid[*robotRow][*robotCol].state = IS_EMPTY; // Vide l'ancienne position
+                *robotRow--; // Met à jour les nouvelles coordonnées
+                grid[*robotRow][*robotCol].state = IS_ROBOT; // Met à jour la nouvelle position du robot
+            }
+            break;
+        case 'q': // aller en bas
+            if (grid[*robotRow - 1][*robotCol].state == IS_EMPTY) {
+                grid[*robotRow][*robotCol].state = IS_EMPTY;
+                (*robotRow)++;
+                grid[*robotRow][*robotCol].state = IS_ROBOT;
+            }
+            break;
+        case 's': // aller à gauche
+            if (grid[*robotRow][*robotCol - 1].state == IS_EMPTY) {
+                grid[*robotRow][*robotCol].state = IS_EMPTY;
+                (*robotCol)--;
+                grid[*robotRow][*robotCol].state = IS_ROBOT;
+            }
+            break;
+        case 'd': // aller à droite
+            if (grid[*robotRow][*robotCol + 1].state == IS_EMPTY) {
+                grid[*robotRow][*robotCol].state = IS_EMPTY;
+                (*robotCol)++;
+                grid[*robotRow][*robotCol].state = IS_ROBOT;
+            }
+            break;
+        default:
+            printf("Invalid direction.\n");
+    }
+}
