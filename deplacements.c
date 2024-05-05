2a7215433f95e4e3b07b3ff3c6fc6ae82873a2f9
valerian <valerian@debian12.myguest.virtualbox.org>
2024-05-05 15:30:10 +0200

    n

--- deplacements.c
+++ deplacements.c
@@ -1,41 +1,44 @@
 #include <stdio.h>
 #include "./struct.h"
 
-void PlayerMovement(CASE** grid,Players robot){ //La fonction prend en paramètres la grille et la structure du joueur pour récupérer ses coordonnées
+void PlayerMovement(CASE **grid, Players robot)
+{ // La fonction prend en paramètres la grille et la structure du joueur pour récupérer ses coordonnées
     int direction;
     printf("Wich direction:\n'z'=UP\n'q'=DOWN\n's'=LEFT\n'd'=RIGHT\n");
-    scanf("%d",&direction);
+    scanf("%d", &direction);
     // Vérification de la direction saisie
-    switch(direction) {
-        case 'z': // aller en haut
-            while (grid[robot.robot_row + 1][robot.robot_col].state == IS_EMPTY) {
-                grid[robot.robot_row][robot.robot_col].state = IS_EMPTY; // Vide l'ancienne position
-                robot.robot_row--; // Met à jour les nouvelles coordonnées
-                grid[robot.robot_row][robot.robot_col].state = IS_ROBOT; // Met à jour la nouvelle position du robot
-            }
-            break;
-        case 'q': // aller en bas
-            while (grid[robot.robot_row - 1][robot.robot_col].state == IS_EMPTY) {
-                grid[robot.robot_row][robot.robot_col].state = IS_EMPTY;
-                (robot.robot_row)++;
-                grid[robot.robot_row][robot.robot_col].state = IS_ROBOT;
-            }
-            break;
-        case 's': // aller à gauche
-            while (grid[robot.robot_row][robot.robot_col - 1].state == IS_EMPTY) {
-                grid[robot.robot_row][robot.robot_col].state = IS_EMPTY;
-                (robot.robot_col)--;
-                grid[robot.robot_row][robot.robot_col].state = IS_ROBOT;
-            }
-            break;
-        case 'd': // aller à droite
-            while (grid[robot.robot_row][robot.robot_col + 1].state == IS_EMPTY) {
-                grid[robot.robot_row][robot.robot_col].state = IS_EMPTY;
-                (robot.robot_col)++;
-                grid[robot.robot_row][robot.robot_col].state = IS_ROBOT;
-            }
-            break;
-        default:
-            printf("Invalid direction.\n");
+    grid[robot.robot_row][robot.robot_col].state = IS_EMPTY; // Vide l'ancienne position
+    switch (direction)
+    {
+    case 'z': // aller en haut
+        while (grid[robot.robot_row + 1][robot.robot_col].state == IS_EMPTY)
+        {
+            robot.robot_row--;                                       // Met à jour les nouvelles coordonnées
+            grid[robot.robot_row][robot.robot_col].state = IS_ROBOT; // Met à jour la nouvelle position du robot
+        }
+        break;
+    case 'q': // aller en bas
+        while (grid[robot.robot_row - 1][robot.robot_col].state == IS_EMPTY)
+        {
+            (robot.robot_row)++;
+            grid[robot.robot_row][robot.robot_col].state = IS_ROBOT;
+        }
+        break;
+    case 's': // aller à gauche
+        while (grid[robot.robot_row][robot.robot_col - 1].state == IS_EMPTY)
+        {
+            (robot.robot_col)--;
+            grid[robot.robot_row][robot.robot_col].state = IS_ROBOT;
+        }
+        break;
+    case 'd': // aller à droite
+        while (grid[robot.robot_row][robot.robot_col + 1].state == IS_EMPTY)
+        {
+            (robot.robot_col)++;
+            grid[robot.robot_row][robot.robot_col].state = IS_ROBOT;
+        }
+        break;
+    default:
+        printf("Invalid direction.\n");
     }
 }
